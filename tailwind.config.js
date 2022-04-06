f0ed9b941d65a232dcb4253427953b57b42cb79c
Lennart-Franck <lennart.franck98@gmail.com>
2022-04-06 09:36:11 +0200

    Set Up with Tailwind and Daisy

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+// tailwind.config.js
+module.exports = {
+  mode: 'jit',
+  content: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['light'],
+  },
+}
+
