f94fc78742ef96b6ff849920002d5842b455d34b
Filip Boron <Filip.boron@yahoo.de>
2022-04-09 11:28:06 +0200

    Link update für Unterkunft anbieten

--- components/ButtonGroupHelp.vue
+++ components/ButtonGroupHelp.vue
@@ -4,7 +4,7 @@
       class="grid grid-cols-2 sm:grid-cols-2 content-center gap-4 md:gap-8 m-2 md:m-8"
     >
       <BaseButton
-        onclick="window.open('https://pdf.form-solutions.net/administrationCenter/Form-Solutions/01001000-0001/consent?redirectId=0a77f17a-f399-4d0e-a17b-db9ffea6eab1&releaseCacheId=5ed2cbeb-e900-46ac-baac-910f84cd74f3', '_blank')"
+        onclick="window.open('https://pdf.form-solutions.net/metaform/Form-Solutions/sid/assistant/62291643c2c1e375d4828c23', '_blank')"
       >
         Unterkunft anbieten
       </BaseButton>
